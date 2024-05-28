62ba4a70f43039604fca63e029b51778fb556843
kavyamandla <kavyamandla2002@gmail.com>
2024-05-28 11:37:37 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,22 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int integerValue = 10;
+        System.out.println("Integer value: " + integerValue);
+
+        
+        double doubleValue = 3.14;
+        System.out.println("Double value: " + doubleValue);
+
+        
+        char charValue = 'A';
+        System.out.println("Character value: " + charValue);
+
+        
+        boolean booleanValue = true;
+        System.out.println("Boolean value: " + booleanValue);
+
+        
+        String stringValue = "Hello, world!";
+        System.out.println("String value: " + stringValue);
+    }
+}
